4376c92f0a2e56dc331aacbf274586ac0426806c
Tungusov Pavel <tungusovpa@gmail.com>
2026-03-25 16:17:17 +0800

    Complete tesk 'Stars'

---
+++ stars/main.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from skimage.measure import label, regionprops
+
+def centroid(labeled, label=1):
+    y, x = np.where(labeled==label)
+    return np.mean(y), np.mean(x)
+
+plus_pat = np.array([[0,0,1,0,0],
+                     [0,0,1,0,0],
+                     [1,1,1,1,1],
+                     [0,0,1,0,0],
+                     [0,0,1,0,0]])
+
+cross_pat = np.array([[1,0,0,0,1],
+                      [0,1,0,1,0],
+                      [0,0,1,0,0],
+                      [0,1,0,1,0],
+                      [1,0,0,0,1]])
+
+image = np.load("stars.npy")
+labeled = label(image)
+stars = 0
+
+for p in regionprops(labeled):
+    img = p.image.astype(int)
+    if np.array_equal(img, plus_pat):
+        stars += 1
+    if np.array_equal(img, cross_pat):
+        stars += 1
+
+print(stars)
+plt.imshow(labeled)
+plt.show()
